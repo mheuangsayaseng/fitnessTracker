8c8dd046cdb01d1a8fab3a3e84ba37e2fd2ec611
mheuangsayaseng <maggieheuang@gmail.com>
2023-05-21 17:50:22 -0500

    maggie

--- db/client.js
+++ db/client.js
@@ -1,5 +1,7 @@
-import { Client } from "pg";
+const { Client } = require("pg");
 
 const client = new Client("postgres://localhost:5432/fitness-dev");
 
 module.exports = client;
+
+console.log("hello!");
