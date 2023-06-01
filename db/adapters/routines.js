de9719c6d77079667396b74ae2b885f58681190c
AliEvanss <schmal.pal17@gmail.com>
2023-06-01 10:35:53 -0500

    added all routine functions, needs a little more work

--- db/adapters/routines.js
+++ db/adapters/routines.js
@@ -6,7 +6,7 @@ async function createRoutine({ creator_id, is_public, name, goal }) {
       rows: [routine],
     } = await client.query(
       `
-        INSERT INTO routines(creator_id, is_public, name, goal)
+        INSERT INTO routine(creator_id, is_public, name, goal)
         VALUES($1, $2, $3, $4)
         ON CONFLICT (name) DO NOTHING
         RETURNING *;
@@ -23,7 +23,7 @@ async function getAllRoutines() {
   try {
     const { rows } = await client.query(
       `
-        SELECT * FROM routines
+        SELECT * FROM routine;
     `
     );
     return rows;
@@ -39,8 +39,8 @@ async function getRoutineById({routineId})
     } = await client.query(
       `
       SELECT creator_id, is_public, name, goal
-      FROM routines
-      WHERE id=${routineId}
+      FROM routine
+      WHERE id=${routineId};
       `);
       if (!routine) {
         return null;
@@ -50,5 +50,124 @@ async function getRoutineById({routineId})
       throw error;
     }
 
+    async function getRoutinesWithoutActivities()
+      try {
+        const {
+          rows: [routine],
+      } = await client.query(
+        `
+        SELECT *
+        FROM routine
+        RETURNING *;
+        `
+      );
 
-module.exports = { createRoutine };
+      return routine();
+
+      } catch (error) {
+        throw error;
+      }
+
+
+    async function getAllPublicRoutines()
+      try {
+        const {
+          rows: [routine],
+        } = await client.query(
+          `
+          SELECT is_public
+          FROM routine 
+          RETURNING *
+          JOIN activities ON activities.id=activities."activitiesId"
+          WHERE activities."activitiesId"=$1;
+          `
+        );
+        return routine(), activities();
+      
+      } catch (error) {
+        throw error;
+      }
+
+
+    //  async function getAllRoutinesByUser(username)
+    //  try {
+    //   const {
+    //     rows: [routine],
+    //   } = await client.query(
+    //     `
+    //     SELECT *
+    //     FROM routine 
+    //     WHERE "routine
+    //     RETURNING *
+    //     JOIN activities ON activities.id=activities."activitiesId"
+    //     WHERE activities."activitiesId"=$1;
+    //     `
+    //   );
+    //   return routine(), activities();
+    
+    // } catch (error) {
+    //   throw error;
+    // }
+
+      
+
+
+
+    async function getPublicRoutinesByUser(username)
+    try {
+      const {
+        rows: [routine],
+      } = await client.query(
+        `
+        SELECT is_public
+        FROM routine 
+        RETURNING *
+        JOIN activities ON activities.id=activities."activitiesId"
+        WHERE activities."activitiesId"=$1;
+        `
+      );
+      return routine(), activities();
+    
+    } catch (error) {
+      throw error;
+    }
+      
+
+
+    async function getPublicRoutinesByActivity(activityId)
+    try {
+      const {
+        rows: [routine],
+      } = await client.query(
+        `
+        SELECT is_public
+        FROM routine, routine_activities
+        JOIN activities ON activities.id=activities."activitiesId"
+        WHERE activities."activitiesId"=$1;
+        `
+      );
+      return routine(), activities();
+    
+    } catch (error) {
+      throw error;
+    }
+      
+
+
+    // async function updateRoutine(routineId, isPublic, name, goal)
+      
+
+
+    // async function destroyRoutine(routineId)
+
+module.exports = { createRoutine, 
+                  getAllRoutines, 
+                  getRoutineById, 
+                  getRoutinesWithoutActivities, 
+                  getAllPublicRoutines, 
+                  getPublicRoutinesByUser, 
+                  // getAllRoutinesByUser, 
+                  getPublicRoutinesByActivity,
+                  // updateRoutine,
+                  // destroyRoutine
+                    };
